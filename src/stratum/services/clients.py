8abf552e5727d90da27931415064ca5eeae88a95
Shaan-kun <shaan-kun@yandex.ru>
2023-05-27 14:50:03 +0400

    добавлена реализация сервиса заказов

--- src/stratum/services/clients.py
+++ src/stratum/services/clients.py
@@ -5,8 +5,8 @@ from fastapi import (
 )
 from sqlalchemy.orm import Session
 
-from ..models import *
-from ..tables import *
+import stratum.models.clients as models
+import stratum.tables as tables
 from ..database import get_session
 
 
@@ -14,5 +14,47 @@ class ClientService:
     def __init__(self, session: Session = Depends(get_session)):
         self.session = session
 
-    def get(self, id: int):
-        pass
+    def _get(self, client_id: int) -> tables.Client:
+        """Возвращает клиента по его id."""
+        client = (
+            self.session
+            .query(tables.Client)
+            .filter(tables.Client.client_id == client_id)
+            .first()
+        )
+
+        if not client:
+            raise HTTPException(status.HTTP_404_NOT_FOUND)
+
+        return client
+
+    def get(self, client_id: int) -> tables.Client:
+        """Возвращает клиента по его id."""
+        return self._get(client_id)
+
+    def get_list(self) -> list[tables.Client]:
+        """Возвращает список клиентов."""
+        clients = (
+            self.session
+            .query(tables.Client)
+            .all()
+        )
+
+        if not clients:
+            raise HTTPException(status.HTTP_404_NOT_FOUND)
+
+        return clients
+
+    def get_by_company(self, company_id: int) -> list[tables.Client]:
+        """Возвращает список клиентов компании по её id."""
+        clients = (
+            self.session
+            .query(tables.Client)
+            .filter(tables.Client.company_id == company_id)
+            .all()
+        )
+
+        if not clients:
+            raise HTTPException(status.HTTP_404_NOT_FOUND)
+
+        return clients
